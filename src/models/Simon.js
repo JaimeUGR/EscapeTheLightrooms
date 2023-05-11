191c84717a85a6800b7e27942d64baa069e83519
franciscquillo <franexca@correo.ugr.es>
2023-05-11 11:57:44 +0200

    Añadiendo modelo de Simon

--- src/models/Simon.js
+++ src/models/Simon.js
@@ -1,22 +1,24 @@
+
 import * as THREE from "../../libs/three.module.js";
+import {TrapezoidGeometry} from "../geometry/TrapezoidGeometry.js";
 
 class Simon extends THREE.Object3D
 {
 	constructor(dimensiones = {
 
-		panelX: 1,
-		panelY: 1,
-		panelZ: 1,
+		panelX: 20,
+		panelY: 30,
+		panelZ: 5,
 
-		radioBotonTop: 1,
-		radioBotonDown: 1,
-		alturaBoton :1,
+		alturaBotonJuego :1,
 
-		borde: 1,
-		radioIndicador: 1,
+		borde: 1.5,
+		radioIndicador: 2,
 		alturaIndicador: 1,
-		separacionBotones: 1
-
+		altoBotonInicial: 1,
+		profundidadBotonInicial: 3,
+		anchoBotonSuperior: 0.8,
+		profBotonSuperior: 0.8
 
 
 	})
@@ -27,96 +29,103 @@ class Simon extends THREE.Object3D
 		this.panelY = dimensiones.panelY
 		this.panelZ = dimensiones.panelZ
 
-		this.radioBotonTop = dimensiones.radioBotonTop
-		this.radioBotonDown = dimensiones.radioBotonDown
-		this.alturaBoton = dimensiones.alturaBoton
+
+		this.alturaBotonJuego = dimensiones.alturaBotonJuego
 
 		this.borde = dimensiones.borde
 		this.radioIndicador = dimensiones.radioIndicador
 		this.alturaIndicador = dimensiones.alturaIndicador
 
-		this.separacionBotones = dimensiones.separacionBotones
+
+		this.altoBotonInicial = dimensiones.altoBotonInicial
+		this.profundidadBotonInicial = dimensiones.profundidadBotonInicial
+
+		this.anchoBotonInicial = (this.panelX - 2*this.borde)
+
+		this.botonAnchoInferior = (this.panelX - 3*this.borde)/2
+		this.botonAnchoSuperior = this.botonAnchoInferior * dimensiones.anchoBotonSuperior
+		this.botonProfInferior = this.botonAnchoInferior
+		this.botonProfSuperior = this.botonProfInferior * dimensiones.profBotonSuperior
+
+		this.separacionCentral = (this.panelY - (5*this.borde + this.profundidadBotonInicial + 2*this.radioIndicador + 2*this.botonProfInferior))/2
+		this.separacionIndicadores = (this.panelX - (2*this.borde + 3*2*this.radioIndicador))/2
+
+		// 2 3
+		// 0 1
+		this.botones = []
+		this.indicadores = [] // 0 1 2
 
 		let simonMaterial = new THREE.MeshNormalMaterial({color: 0Xf1f1f1,opacity: 0.5,transparent: true})
 
 		// Crear panel de soporte
 		let geoPanel = new THREE.BoxGeometry(this.panelX,this.panelY,this.panelZ)
-		geoPanel.translate(0,this.panelY/2,this.panelZ/2)
+		geoPanel.translate(0,this.panelY/2,-this.panelZ/2)
 		this.add(new THREE.Mesh(geoPanel,simonMaterial))
 
+		//Crear boton inicial
+		let geoBotonInicial = new TrapezoidGeometry(this.anchoBotonInicial, this.profundidadBotonInicial, this.anchoBotonInicial, this.profundidadBotonInicial, this.altoBotonInicial)
+		geoBotonInicial.rotateX(Math.PI/2)
+		geoBotonInicial.translate(0,this.borde + this.profundidadBotonInicial/2,this.altoBotonInicial/2)
 
-		// Crear indicador
-		let geoIndicador1 = new THREE.CylinderGeometry(this.radioIndicador,this.radioIndicador,this.alturaIndicador,12)
-		geoIndicador1.rotateX(-Math.PI/2)
-		geoIndicador1.translate(0,this.alturaIndicador/2 + (this.panelY-this.borde) ,this.panelZ)
+		this.botonInicialMesh = new THREE.Mesh(geoBotonInicial, new THREE.MeshBasicMaterial({color: 0x556655}))
 
-		let geoIndicador2 = geoIndicador1.clone()
-		geoIndicador2.translate(-this.borde,0,0)
+		this.add(this.botonInicialMesh)
 
-		let geoIndicador3 = geoIndicador1.clone()
-		geoIndicador3.translate(this.borde,0,0)
+		this.materialesBotonesJuego = [
+			new THREE.MeshLambertMaterial({color: 0x32a852 }),
+			new THREE.MeshLambertMaterial({color: 0x32a0a8 }),
+			new THREE.MeshLambertMaterial({color: 0x9aa832 }),
+			new THREE.MeshLambertMaterial({color: 0xa83232 })
+		]
 
+		// Crear botones de juego
+		let geoBotonJuego = new TrapezoidGeometry(this.botonAnchoInferior, this.botonProfInferior, this.botonAnchoSuperior, this.botonProfSuperior, this.alturaBotonJuego)
+		geoBotonJuego.rotateX(Math.PI/2)
+		geoBotonJuego.translate(0,0,this.alturaBotonJuego/2)
 
-		this.add(new THREE.Mesh(geoIndicador1,simonMaterial))
-		this.add(new THREE.Mesh(geoIndicador2,simonMaterial))
-		this.add(new THREE.Mesh(geoIndicador3,simonMaterial))
+		let botonJuego = new THREE.Mesh(geoBotonJuego, this.materialesBotonesJuego[0] )
+		botonJuego.position.set(this.botonAnchoInferior/2 + this.borde - this.panelX/2,
+			2*this.borde + this.profundidadBotonInicial + this.separacionCentral + this.botonProfInferior/2, 0)
 
+		this.botones.push(botonJuego)
 
+		botonJuego = botonJuego.clone()
+		botonJuego.material = this.materialesBotonesJuego[1]
+		botonJuego.position.x += this.borde + this.botonAnchoInferior
 
-		// Crear botones
-		let geoBoton1 = new THREE.CylinderGeometry(this.radioBotonTop,this.radioBotonDown,this.alturaBoton,4)
-		geoBoton1.rotateY(Math.PI/4)
-		geoBoton1.rotateX(Math.PI/2)
-		geoBoton1.translate(0,0,this.alturaBoton/2)
-		//geoBoton1.scale(1,1,0.5)
+		this.botones.push(botonJuego)
 
+		botonJuego = botonJuego.clone()
+		botonJuego.material = this.materialesBotonesJuego[2]
+		botonJuego.position.x -= (this.borde + this.botonAnchoInferior)
+		botonJuego.position.y += this.borde + this.botonProfInferior
 
-		let geoBoton2 = new THREE.CylinderGeometry(this.radioBotonTop,this.radioBotonDown,this.alturaBoton,4)
-		geoBoton2.rotateY(Math.PI/4)
-		geoBoton2.rotateX(Math.PI/2)
-		geoBoton2.translate(0,0,this.alturaBoton/2)
+		this.botones.push(botonJuego)
 
+		botonJuego = botonJuego.clone()
+		botonJuego.material = this.materialesBotonesJuego[3]
 
+		this.botones.push(botonJuego)
+		botonJuego.position.x += this.borde + this.botonAnchoInferior
 
-		let geoBoton3 = new THREE.CylinderGeometry(this.radioBotonTop,this.radioBotonDown,this.alturaBoton,4)
+		for (let i = 0; i < this.botones.length; i++)
+			this.add(this.botones[i])
 
-		geoBoton3.rotateY(Math.PI/4)
-		geoBoton3.rotateX(Math.PI/2)
-		geoBoton3.translate(0,0,this.alturaBoton/2)
+		let geoIndicador = new THREE.CylinderGeometry(this.radioIndicador, this.radioIndicador, this.alturaIndicador)
+		geoIndicador.rotateX(Math.PI/2)
+		geoIndicador.translate(-this.panelX/2, this.panelY, this.alturaIndicador/2) // Esquina sup izquierda centrado
 
+		// Colocarlos en su posición
+		geoIndicador.translate(this.radioIndicador + this.borde, -(this.radioIndicador + this.borde), 0)
+		this.indicadores.push(new THREE.Mesh(geoIndicador.clone(), new THREE.MeshBasicMaterial({color: 0x111111})))
 
+		geoIndicador.translate(2*this.radioIndicador + this.separacionIndicadores, 0, 0)
+		this.indicadores.push(new THREE.Mesh(geoIndicador.clone(), new THREE.MeshBasicMaterial({color: 0x111111})))
 
-		let geoBoton4 = new THREE.CylinderGeometry(this.radioBotonTop,this.radioBotonDown,this.alturaBoton,4)
-		geoBoton4.rotateY(Math.PI/4)
-		geoBoton4.rotateX(Math.PI/2)
-		geoBoton4.translate(0,0,this.alturaBoton/2)
+		geoIndicador.translate(2*this.radioIndicador + this.separacionIndicadores, 0, 0)
+		this.indicadores.push(new THREE.Mesh(geoIndicador.clone(), new THREE.MeshBasicMaterial({color: 0x111111})))
 
-
-		this.botonMesh1 = new THREE.Mesh(geoBoton1, simonMaterial)
-	    this.botonMesh1.position.set(-this.radioBotonDown/2 - this.radioBotonTop, this.radioBotonTop/2 + this.radioBotonDown/2 + 2*this.borde, this.panelZ)
-		this.botonMesh1.scale.set(1,1,0.5)
-
-		this.botonMesh2 = new THREE.Mesh(geoBoton2, simonMaterial)
-		this.botonMesh2.position.set(this.radioBotonDown/2 + this.radioBotonTop, this.radioBotonDown/2+this.radioBotonTop/2 + 2* this.borde, this.panelZ)
-
-		this.botonMesh3 = new THREE.Mesh(geoBoton3, simonMaterial)
-		this.botonMesh3.position.set(this.radioBotonDown/2 + this.radioBotonTop, this.radioBotonDown/2+this.radioBotonTop/2 + this.borde, this.panelZ)
-
-		this.botonMesh4 = new THREE.Mesh(geoBoton4, simonMaterial)
-		this.botonMesh4.position.set(-this.radioBotonDown/2 - this.radioBotonTop, this.radioBotonDown/2+this.radioBotonTop/2 + this.borde, this.panelZ)
-
-
-
-
-		this.add(this.botonMesh1)
-		this.add(this.botonMesh2)
-		this.add(this.botonMesh3)
-		this.add(this.botonMesh4)
-
-
-
-
-
-
+		for (let i = 0; i < this.indicadores.length; i++)
+			this.add(this.indicadores[i])
 	}
 }export {Simon}
